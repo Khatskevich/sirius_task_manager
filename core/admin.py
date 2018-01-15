c651f34f7a929299fd15ba1e843afdd223fb0817
Vladimir Panov <vladimir.panov@phystech.edu>
2018-01-15 21:45:28 +0300

    UserAdmin model

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import ugettext_lazy as _
 
 # Register your models here.
 from .models import User
