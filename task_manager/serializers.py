b19e18b901b408873a3e3754b06cd4e3f68897fd
AKhatskevich <avkhatskevich@gmail.com>
2018-01-21 15:56:20 +0300

    fix bug: owner required

--- task_manager/serializers.py
+++ task_manager/serializers.py
@@ -8,4 +8,5 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
+        read_only_fields = ("owner", "id", "created_at")
         fields = '__all__'
